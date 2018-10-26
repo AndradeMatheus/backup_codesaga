2550fcdd15ce17de2ffd90a3f2823d4221098416
Matheus Andrade <matheus.andrade.santos@gmail.com>
2018-10-26 15:29:05 -0300

    Segundo exercício: Métodos

--- task_list.rb
+++ task_list.rb
@@ -1,14 +1,20 @@
 opcao = 3
 
+def menu()
+
+  puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+  puts '[1] Inserir uma tarefa'
+  puts '[2] Ver todas as tarefas'
+  puts "[0] Sair \n"
+  print 'Opção escolhida: '
+
+  resposta = gets.to_i
+  return resposta
+end
+
 while opcao != 0 do
 
-	puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
-	puts '[1] Inserir uma tarefa'
-	puts '[2] Ver todas as tarefas'
-	puts "[0] Sair \n"
-	print 'Opção escolhida: '
-
-	opcao = gets.to_i
+	opcao = menu()
 	tarefa = ""
 
 	system('clear')
